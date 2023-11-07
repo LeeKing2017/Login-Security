6de0a5ae3e1ae929da76a46fa07129dd5e200a65
LeeKing2017 <thrusum123@gmail.com>
2023-11-08 00:28:54 +0900

    FEAT<Member>:member entity 생성

--- src/main/java/web/LoginSecurity/LoginSecurityApplication.java
+++ src/main/java/web/LoginSecurity/LoginSecurityApplication.java
@@ -2,8 +2,10 @@ package web.LoginSecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LoginSecurityApplication {
 
 	public static void main(String[] args) {
--- src/main/java/web/LoginSecurity/member/domain/Member.java
+++ src/main/java/web/LoginSecurity/member/domain/Member.java
@@ -1,4 +1,32 @@
 package web.LoginSecurity.member.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EqualsAndHashCode
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @CreatedDate
+    private LocalDateTime createTime;
+
+    @Builder
+    public Member(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
